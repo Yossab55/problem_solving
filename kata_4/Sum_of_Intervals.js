43ffdeeed7a648e23561b563221608a745d4a656
Yossab_Samuel <yossabpro5@gmail.com>
2025-03-03 06:45:35 +0200

    make the code of problem clean

--- kata_4/Sum_of_Intervals.js
+++ kata_4/Sum_of_Intervals.js
@@ -3,24 +3,22 @@ function sumIntervals(intervals) {
     let bigNumber = intervals[i][1];
     for (let j = 0; j < intervals.length; j++) {
       let checkedInterval = intervals[j];
-      if (
-        bigNumber > checkedInterval[0] &&
-        bigNumber <= checkedInterval[1] &&
-        j != i
-      ) {
+      if (checkBetween(bigNumber, checkedInterval) && j != i) {
         intervals[i][1] = checkedInterval[1];
-        intervals = deleteBetweenInterval(intervals, whoToDelete(intervals, i , j));
+        intervals = deleteBetweenInterval(
+          intervals,
+          whoToDelete(intervals, i, j)
+        );
         i--;
         break;
       }
     }
   }
-  let length = 0;
-  console.log(intervals)
-  for(let i = 0; i < intervals.length; i++) {
-    length += intervals[i][1] - intervals[i][0];
-  }
-  return length;
+  return getLength(intervals);
+}
+function checkBetween(number, interval) {
+  if (number > interval[0] && number <= interval[1]) return true;
+  return false;
 }
 function deleteBetweenInterval(array, index) {
   return array.slice(0, index).concat(array.slice(index + 1));
@@ -28,7 +26,14 @@ function deleteBetweenInterval(array, index) {
 function whoToDelete(array, i, j) {
   let lengthOfI = array[i][1] - array[i][0];
   let lengthOfJ = array[j][1] - array[j][0];
-  return (lengthOfI >= lengthOfJ) ? j : i;
+  return lengthOfI >= lengthOfJ ? j : i;
+}
+function getLength(intervals) {
+  let length = 0;
+  for (let i = 0; i < intervals.length; i++) {
+    length += intervals[i][1] - intervals[i][0];
+  }
+  return length;
 }
 console.log(
   sumIntervals([
@@ -37,4 +42,12 @@ console.log(
     [3, 5],
   ])
 );
-console.log(sumIntervals([[1, 5], [10, 20], [1, 6], [16, 19], [5, 11]]));
+console.log(
+  sumIntervals([
+    [1, 5],
+    [10, 20],
+    [1, 6],
+    [16, 19],
+    [5, 11],
+  ])
+);
